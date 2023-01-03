97b11343baa6a57902267c1c106ea5557a4d00ae
shubhamgohad221997 <shubhamgohad221997@gmail.com>
2023-01-03 20:52:49 +0530

    Add_Notice_Backend

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const connect=require("./Connect/Connect")
+
+const userRouter=require("./Controller/User.Router")
+const cors = require("cors")
+const noticeRoute=require("./Controller/Notice.Router")
+const { default: mongoose } = require('mongoose')
+mongoose.set('strictQuery', true)
+
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+
+// app.use("/login",userRouter)
+
+app.use("/notice", noticeRoute)
+
+
+app.get('/', (req, res) => res.send('hello'))
+
+
+
+app.listen(8080, async () => {
+    await connect()
+    console.log('server started on port 8080')
+
+})
